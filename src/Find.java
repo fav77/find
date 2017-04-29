af5004af9dfa7d9d7edb3931b43d929c0cb2fb26
Igorlo <9440533@gmail.com>
2017-04-29 19:58:42 +0300

    find

---
+++ src/Find.java
@@ -0,0 +1,53 @@
+import java.io.File;
+
+public class Find {
+    private String directory;
+
+    private Boolean useSubdirectory;
+
+    public Find(String directory, Boolean useSubdirectory) {
+        this.directory = directory;
+        this.useSubdirectory = useSubdirectory;
+    }
+
+    private String searchin(String dir, String fileName) {
+        File file = new File(dir);
+        String[] dirs = file.list();
+        for (int i = 0; i < dirs.length; i++) {
+            String fullName;
+            if (dirs[i].equals(fileName)) {
+                fullName = directory + "\\" + dirs[i];
+            } else {
+                fullName = directory + "\\" + dirs[i] + "\\" + fileName;
+            }
+            if (new File(fullName).isFile()) {
+                return fullName;
+            }
+            fullName = searchin(fullName, fileName);
+        }
+        return "ERROR";
+    }
+
+    private String search(String dir, String fileName) {
+        File file = new File(dir);
+        String fullName = dir + "\\" + fileName;
+        if (new File(fullName).isFile()) {
+            return fullName;
+        }
+        return "ERROR";
+    }
+
+    public File find(String fileName){
+        String result = "";
+        File file = new File(directory);
+        if(useSubdirectory){
+            result = searchin(directory, fileName);
+        }
+        else{
+            result = search(directory, fileName);
+        }
+        //if (result.equals("ERROR")) Завершить
+
+        return new File(result);
+    }
+}
---
+++ src/FindLauncher.java
@@ -0,0 +1,46 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.*;
+
+public class FindLauncher {
+
+        @Option(name = "-d", metaVar = "directory", usage = "find in this directory")
+        private String outputFile;
+
+        @Option(name = "-r", metaVar = "true/false", usage = "use subdirectories")
+        private Boolean useSubdirectories = false;
+
+        @Argument(metaVar = "filename", usage = "Input file name")
+        private String fileName;
+
+
+        public static void main(String[] args) throws Exception {
+            new FindLauncher().launch(args);
+        }
+
+        private void launch(String[] args) throws Exception {
+            CmdLineParser parser = new CmdLineParser(this);
+
+            try {
+                parser.parseArgument(args);
+            } catch (CmdLineException e) {
+                System.err.println(e.getMessage());
+                System.err.println("java -jar Find.jar -d directory -r true/false fileName");
+                parser.printUsage(System.err);
+                return;
+            }
+            Find find = new Find(outputFile, useSubdirectories);
+            try (BufferedReader reader = new BufferedReader(new FileReader(find.find(fileName)))){
+                String line = reader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+
+            }
+            System.out.println("Done") ;
+        }
+    }
